f3c72717c5574958d2c3b9de5af172b2c911523d
Eto <pavlinpalev69@gmail.com>
2025-10-05 23:24:55 +0100

    randomiser roleplay actions changed cause i was tired as shit yesterday

--- randomisers/Roleplay.js
+++ randomisers/Roleplay.js
@@ -1,5 +1,5 @@
 const randomRoleplayaction = [
-	'99 Nights in the Forest',
+	'Fuck',
 ]
 const selectedRoleplayaction = Math.floor(Math.random() * randomRoleplayaction.length);
 console.log(randomRoleplayaction[selectedRoleplayaction]);
